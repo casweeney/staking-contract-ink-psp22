4bca804511125359af09594e678ea27e5bd8ed11
casweeney <casweeno2000@gmail.com>
2024-01-11 06:17:40 +0100

    cleaned contracts lib.rs

--- contracts/lib.rs
+++ contracts/lib.rs
@@ -1,44 +1,21 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[ink::contract]
+#[openbrush::contract]
 mod staking_contract {
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct StakingContract {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        
     }
 
     impl StakingContract {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {}
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
         pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
         }
     }
 
@@ -78,65 +55,6 @@ mod staking_contract {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
 
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = StakingContractRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("staking_contract", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<StakingContractRef>(contract_account_id.clone())
-                .call(|staking_contract| staking_contract.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = StakingContractRef::new(false);
-            let contract_account_id = client
-                .instantiate("staking_contract", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<StakingContractRef>(contract_account_id.clone())
-                .call(|staking_contract| staking_contract.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<StakingContractRef>(contract_account_id.clone())
-                .call(|staking_contract| staking_contract.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<StakingContractRef>(contract_account_id.clone())
-                .call(|staking_contract| staking_contract.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
-        }
     }
 }
