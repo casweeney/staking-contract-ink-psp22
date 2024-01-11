2fcd8d5fa48bba4c210b28194b6740bd1463c4ef
casweeney <casweeno2000@gmail.com>
2024-01-11 07:40:36 +0100

    PSP22 token contract completed

--- contracts/lib.rs
+++ contracts/lib.rs
@@ -1,21 +1,34 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+#[openbrush::implementation(PSP22)]
 #[openbrush::contract]
 mod staking_contract {
+    // use openbrush::contracts::psp22::*;
+    use openbrush::traits::Storage;
+    use openbrush::test_utils::*;
+    use openbrush::traits::String;
 
     #[ink(storage)]
+    #[derive(Storage, Default)]
     pub struct StakingContract {
-        
+        #[storage_field]
+        psp22: psp22::Data
+    }
+
+    #[overrider(psp22::Internal)]
+    fn _before_token_transfer(&mut self, from: Option<&AccountId>, to: Option<&AccountId>, amount: &Balance) -> Result<(), PSP22Error> {
+        if from == Some(&[0; 32].into()) {
+            return Err(PSP22Error::Custom(String::from("Transfer from zero address not allowed")));
+        }
+        Ok(())
     }
 
     impl StakingContract {
         #[ink(constructor)]
-        pub fn new() -> Self {
-            Self {}
-        }
-
-        #[ink(message)]
-        pub fn flip(&mut self) {
+        pub fn new(total_supply: Balance) -> Self {
+            let mut contract = Self::default();
+            psp22::Internal::_mint_to(&mut contract, Self::env().caller(), total_supply).expect("Could not mint");
+            contract
         }
     }
 
@@ -27,34 +40,33 @@ mod staking_contract {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// We test if the default constructor does its job.
         #[ink::test]
-        fn default_works() {
-            let staking_contract = StakingContract::default();
-            assert_eq!(staking_contract.get(), false);
+        fn constructor_works() {
+            let accounts = accounts();
+            let mint_amount = 10_000_000;
+
+            let staking_contract = StakingContract::new(mint_amount);
+
+            let alice_balance = PSP22::balance_of(&staking_contract, accounts.alice);
+
+            assert_eq!(alice_balance, mint_amount);
         }
 
-        /// We test a simple use case of our contract.
         #[ink::test]
-        fn it_works() {
-            let mut staking_contract = StakingContract::new(false);
-            assert_eq!(staking_contract.get(), false);
-            staking_contract.flip();
-            assert_eq!(staking_contract.get(), true);
+        fn transfer_works() {
+            let accounts = accounts();
+            let mint_amount = 10_000_000;
+            let transfer_amount = 1_000;
+
+            let mut staking_contract = StakingContract::new(mint_amount);
+            let result = PSP22::transfer(&mut staking_contract, accounts.bob, transfer_amount, Vec::<u8>::new());
+
+            let alice_balance = PSP22::balance_of(&staking_contract, accounts.alice);
+            let bob_balance = PSP22::balance_of(&staking_contract, accounts.bob);
+
+            assert!(result.is_ok());
+            assert_eq!(alice_balance, mint_amount - transfer_amount);
+            assert_eq!(bob_balance, transfer_amount);
         }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-
     }
 }
